675a0501c4ecd47a642e9fc54fd35e7179a7c9e4
Daniel Sportes <daniel@sportes.fr>
2026-01-25 18:49:21 +0100

    mysafe.sql

---
+++ mysql-safe/mysafe.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : dim. 25 jan. 2026 à 11:31
+-- Version du serveur : 8.0.44-0ubuntu0.24.04.2
+-- Version de PHP : 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `mysafe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `properties`
+--
+
+CREATE TABLE `properties` (
+  `N1` varchar(64) NOT NULL,
+  `N2` varchar(64) NOT NULL,
+  `N3` varchar(64) NOT NULL,
+  `N4` varchar(64) NOT NULL,
+  `VAL` varchar(10000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `properties`
+--
+ALTER TABLE `properties`
+  ADD PRIMARY KEY (`N1`,`N2`,`N3`,`N4`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
